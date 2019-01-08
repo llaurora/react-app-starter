27fde75e232fffb0d5b354f319e008ee60901964
DxLucky <flovekind@163.com>
2019-01-08 17:58:20 +0800

    移除react-particles-js

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Particles from "react-particles-js";
+// import Particles from "react-particles-js";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
