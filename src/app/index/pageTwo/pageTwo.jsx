5c101fe2a48ca197c1c90e7b840f0f3aeb2372ce
DxLucky <flovekind@163.com>
2019-03-24 18:18:10 +0800

    升级到webpack4,并升级React版本到16.7

--- src/app/index/pageTwo/pageTwo.jsx
+++ src/app/index/pageTwo/pageTwo.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 class PageTwo extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            count:0
+        }
+    }
+    add=()=>{
+        this.setState({
+            count:this.state.count+1
+        })
+    };
     render(){
+        const {count}=this.state;
         return(
-            <h1>这是第二页22222</h1>
+            <div>
+                <h1>这是第二页22222</h1>
+                <hr/>
+                <h2>现在计数为{count}</h2>
+                <button onClick={this.add}>Count+</button>
+                <h2>dff</h2>
+            </div>
         )
     }
 }
