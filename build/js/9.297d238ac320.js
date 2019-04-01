e76560acb5fb23e389caf3e842c864ead0492e66
DxLucky <flovekind@163.com>
2019-04-01 11:36:23 +0800

    升级依赖，增加polyfill和runtime对复杂语法的转换

--- build/js/9.297d238ac320.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{49:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return c});var u=t(0),r=t.n(u);function c(){return r.a.createElement("h3",null,"这是第二页")}}}]);
