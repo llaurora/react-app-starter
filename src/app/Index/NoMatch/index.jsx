749a7bffd12a9d41e59ef04179936cb084689599
DxLucky <flovekind@163.com>
2019-07-26 11:37:18 +0800

    feat: 增加404页面

---
+++ src/app/Index/NoMatch/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function NoMatch({ location: { pathname } }) {
+  return (
+    <div>
+      <h3>
+        No match for <code>{pathname}</code>
+      </h3>
+    </div>
+  );
+}
+
+NoMatch.propTypes = {
+  location: PropTypes.object,
+};
