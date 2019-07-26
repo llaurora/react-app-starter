759418adea53f86c4c440e212f05bd32978d2c80
DxLucky <flovekind@163.com>
2019-07-26 11:37:18 +0800

    feat: 增加404页面

--- src/app/Index/index.jsx
+++ src/app/Index/index.jsx
@@ -1,10 +1,11 @@
 import React, { lazy } from 'react';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Aside from './components/Aside';
 import './style.scss';
 
 const Home = lazy(() => import('./Home')); // 登录后主页
+const NoMatch = lazy(() => import('./NoMatch')); // 404没有匹配页面
 const PageOne = lazy(() => import('./PageOne')); // 第一页
 const PageTwo = lazy(() => import('./PageTwo')); // 第二页
 const PageThree = lazy(() => import('./PageThree')); // 第三页
@@ -18,14 +19,17 @@ export default function Index({ history, location }) {
     <div id="indexArea">
       <Aside history={history} location={location} />
       <div id="routeContent">
-        <Route exact path="/" component={Home} />
-        <Route path="/pageone" component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
-        <Route path="/pagethree" component={PageThree} />
-        <Route path="/pagefour" component={PageFour} />
-        <Route path="/pagefive" component={PageFive} />
-        <Route path="/pagesix" component={PageSix} />
-        <Route path="/pageseven" component={PageSeven} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/pageone" component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/pagethree" component={PageThree} />
+          <Route path="/pagefour" component={PageFour} />
+          <Route path="/pagefive" component={PageFive} />
+          <Route path="/pagesix" component={PageSix} />
+          <Route path="/pageseven" component={PageSeven} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     </div>
   );
