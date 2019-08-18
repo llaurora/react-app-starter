45751d4c138e3745570e109308b591d231f791e2
DlLucky <flovekind@163.com>
2019-08-18 12:57:45 +0800

    perf: 优化登录用户信息Reducer

--- src/redux/Login/loginReducer.js
+++ src/redux/Login/loginReducer.js
@@ -1,10 +1,11 @@
-const initialState = { isLogin: false, userInfo: {} };
+const initialState = { isLogin: false, userName: '' };
 export default function(state = initialState, action) {
   switch (action.type) {
     case 'CANCEL_LOGIN_STATE':
       return {
         ...state,
-        ...{ isLogin: !state.isLogin, userInfo: action.data },
+        isLogin: true,
+        userName: action.data.userName,
       };
     case 'CLEAR_LOGIN_STATE':
       return initialState;
