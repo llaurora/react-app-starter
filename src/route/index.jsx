c6f790c82847ebe6e3eaaadcf766dda2e59c1b24
llaurora <llaurorae@gmail.com>
2019-11-20 11:42:41 +0800

    perf: 优化懒加载loading交互

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -5,36 +5,18 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { store, persistor } from './configureStore';
 import { AuthorizedLogin } from '@/components/Authorized';
-import globalLoading from '@/asset/images/globalLoading.gif';
+import Loading from '@/components/Loading';
 import '@/asset/styles/global.scss';
 
 const Login = lazy(() => import('@/pages/Login')); // 登录页面
 const Index = lazy(() => import('@/pages/Index')); // 登录后主页
-
-const Loading = () => {
-  const style = {
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-  return (
-    <div style={{ ...style }}>
-      <img src={globalLoading} alt="加载中..." />
-    </div>
-  );
-};
 
 const supportsHistory = 'pushState' in window.history;
 
 const Routers = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Suspense fallback={<Loading />}>
+      <Suspense fallback={<Loading scope="global" />}>
         <Router forceRefresh={!supportsHistory}>
           <Switch>
             <Route path="/login" component={Login} />
