1de98f808ccf91050fafee1b9ff830ecf60e98ab
llaurora <llaurorae@gmail.com>
2023-05-23 14:22:26 +0800

    chore: 更新stylelint相关npm包版本

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
-    extends: ["stylelint-config-standard", "stylelint-config-rational-order", "stylelint-config-prettier"],
-    plugins: ["stylelint-scss", "stylelint-order"],
+    extends: ["stylelint-config-standard", "stylelint-prettier/recommended", "stylelint-config-rational-order",],
+    plugins: ["stylelint-scss"],
+    customSyntax: "postcss-scss",
     rules: {
-        "comment-empty-line-before": null,
-        "function-name-case": ["lower", { ignoreFunctions: ["/colorPalette/"] }],
-        "no-invalid-double-slash-comments": null,
-        "no-descending-specificity": null,
-        "declaration-empty-line-before": null,
-        "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["global", "local"] }],
+        "selector-class-pattern": null,
         "at-rule-no-unknown": null,
         "scss/at-rule-no-unknown": true,
+        "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["global", "local"] }],
     },
 };
